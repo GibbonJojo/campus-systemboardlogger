02639e7fd8b655bf1ec3541b1d81e3d4a638548f
GibbonJojo <47012283+GibbonJojo@users.noreply.github.com>
2019-11-22 16:26:49 +0100

    changes for heroku

--- find_rects.py
+++ find_rects.py
@@ -1,4 +1,7 @@
+!pip install opencv-python
+!apt update && apt install -y libsm6 libxext6
 import cv2
+
 
 # Function written by Stackoverflow-User David Tran
 def DED(grayImg):                           #Edge Detection, returns image array
